11b09d1e449c4f19ac16fcbf0772535b4052142a
Randolph Davis Hill <rdhillbb@icloud.com>
2019-03-30 21:36:44 +0700

    Initial Commit

---
+++ Udder-GIT-LESSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder-GIT-LESSON
+//
+//  Created by Randolph Davis Hill on 30/3/19.
+//  Copyright © 2019 Randolph Davis Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
